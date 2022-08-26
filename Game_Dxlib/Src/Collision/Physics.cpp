ed2e87db7cbbe2ff85e541d0c97be48303f12964
Shota Yamamoto <g021g1055@g.neec.ac.jp>
2022-08-26 12:07:41 +0900

    押し戻しの作成。回転をどうにかします。

--- Game_Dxlib/Src/Collision/Physics.cpp
+++ Game_Dxlib/Src/Collision/Physics.cpp
@@ -28,19 +28,19 @@ void Physics::Update(float delta_time)
 	//重力
 	velocity_.y += Gravity * delta_time;
 
-	//衝突計算
-	CollideField();
-	
 	//速度系計算
 	//velocity_ = velocity_*(1-(0.925f * delta_time));
 	//anguler_velocity_ = anguler_velocity_ *(1-( 0.8f * delta_time));
-
+	
 	//トランスフォーム反映
 	transform_->translate(velocity_, GStransform::Space::World);
 	transform_->rotate(
 		GSvector3{ anguler_velocity_.z,0.0f,anguler_velocity_.x }
 		/ (2.0f * GS_PI * sphere_->radius) * 360.0f,
 		GStransform::Space::World);
+
+	//衝突計算
+	CollideField();
 }
 
 void Physics::AddForce(GSvector3 power)
@@ -104,15 +104,23 @@ void Physics::CollideField()
 		}
 
 		//デバッグ
-		Debug::Log(normV);
-		Debug::Log(velocity_);
+		//Debug::Log(normV);
+		//Debug::Log(velocity_);
 
 		//反射ベクトル(F=F+2aN)
 		velocity_ = velocity_-(2*GSvector3::dot(velocity_,normV)*normV);
 		//反発係数をもとに減衰
 		velocity_ *= MAX(0.001f,CLAMP(((bounciness_ + world_->field()->Bounciness) / 2), 0.0f, 1.0f));
 		
-		
+		//押し戻し
+		Line l{ transform_->position(), transform_->position()-normV*(sphere_->radius+0.1f) };
+		MV1_COLL_RESULT_POLY poly;
+		if (world_->field()->collide(l, &poly)) 
+		{
+			GSvector3 position{ poly.HitPosition };
+			position += normV * (sphere_->radius);
+			transform_->position(position);
+		}
 	}
 	DxLib::MV1CollResultPolyDimTerminate(polydim);
 }
